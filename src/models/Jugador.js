9b4a818547ff7d4e117d56e75cef1aaa00d14a54
iago0 <posseiago@gmail.com>
2022-10-23 15:02:07 +0200

    getters y setters clases

--- src/models/Jugador.js
+++ src/models/Jugador.js
@@ -1,9 +1,32 @@
-module.exports = class Usuario {
+module.exports = class Jugador {
   constructor (nombre, username, password){
     this.nombre = nombre;
     this.username = username;
     this.password = password;
   }
 
-  
+
+  getNombre() {
+    return this.nombre;
+  }
+
+  getUsername() {
+    return this.numero;
+  }
+
+  getPassword(){
+    return this.password
+  }
+
+  setNombre(nuevoNombre){
+    this.nombre = nuevoNombre;
+  }
+
+  setUsername(nuevoUsername){
+    this.username = nuevoUsername;
+  }
+
+  setPassword(nuevoPassword){
+    this.password = nuevoPassword;
+  }
 }
