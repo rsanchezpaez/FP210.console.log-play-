2d7edc78ff0390e14958ac86ec9d0df1d5c985a3
iago0 <posseiago@gmail.com>
2022-10-23 14:20:00 +0200

    añadidas clases modelo

---
+++ src/models/Jugador.js
@@ -0,0 +1,9 @@
+module.exports = class Usuario {
+  constructor (nombre, username, password){
+    this.nombre = nombre;
+    this.username = username;
+    this.password = password;
+  }
+
+  
+}
---
+++ src/models/Partida.js
@@ -0,0 +1,12 @@
+class Partida {
+  constructor (numero, sala, usuario1, usuario2){
+    this.numero = numero;
+    this.sala = sala;
+    this.usuario1 = usuario1;
+    this.usuario2 = usuario2;
+  }
+  
+}
+
+
+module.exports = Partida;
