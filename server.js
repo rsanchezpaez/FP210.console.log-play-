3a9a0ccc86af113e055b3a988ddb4b0131b3656a
iago0 <posseiago@gmail.com>
2022-10-26 21:29:57 +0200

    clases

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 var http = require("http");
 var url = require("url");
 
+
 function init(route, handle){
     function onRequest(request, response){
         var pathname = url.parse(request.url).pathname;
