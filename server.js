70a5f26ccf1ed699ff615a2e5253d4a3f6e42d4a
Renata Sánchez <rsanchezpae@uoc.edu>
2022-11-09 19:15:48 +0000

    Update home.html and server.js

--- server.js
+++ server.js
@@ -1,30 +1,27 @@
-
 //SERVIDOR HTTP BASICO
 //inicio del servidor
 
 var http = require("http");
 var url = require("url");
 
+function init(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    var idpath = url.parse(request.url).query;
+    var postData = "";
 
-function init(route, handle){
-    function onRequest(request, response){
-        var pathname = url.parse(request.url).pathname;
-        var idpath = url.parse(request.url).query;
-        var postData = "";
+    request.setEncoding("utf-8");
+    request.addListener("data", function (textPost) {
+      postData += textPost;
+      console.log("text post recived: " + textPost + ".");
+    });
 
-        request.setEncoding("utf-8");
-        request.addListener("data", function(textPost){
-            postData += textPost;
-            console.log("text post recived: " + textPost + ".");
-        });
+    request.addListener("end", function () {
+      route(handle, pathname, response, postData, idpath);
+    });
+  }
 
-        request.addListener("end", function(){
-            route(handle, pathname, response, postData, idpath);
-        });
-
-    }
-    
-    http.createServer(onRequest).listen(8080);
-} 
+  http.createServer(onRequest).listen(8888);
+}
 
 exports.init = init;
